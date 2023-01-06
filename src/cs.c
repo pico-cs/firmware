f1cd2e8e85d7a56537fc09adce4693e6cfbb1b32
stfnmllr <stefan.miller@sap.com>
2023-01-06 16:21:41 +0100

    firmware v0.6.3

--- src/cs.c
+++ src/cs.c
@@ -11,7 +11,7 @@
 #include "channel.h"
 #include "loop.h"
 
-#define PROGRAM_VERSION     "v0.6.2"
+#define PROGRAM_VERSION     "v0.6.3"
 #define PROGRAM_DESCRIPTION "pico-cs DCC command station"
 #define PROGRAM_URL         "https://github.com/pico-cs"
 
